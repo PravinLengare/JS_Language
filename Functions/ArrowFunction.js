d188279a71284a064623b8f617cc7cbdf21a4f76
PravinLengare <lengarepravin115@gmail.com>
2026-01-16 09:58:20 +0530

    Call stacks and execution of js

--- Functions/ArrowFunction.js
+++ Functions/ArrowFunction.js
@@ -15,6 +15,16 @@ console.log(this); // it gives empty object
 
 /**
  * We can't use the this in the function
+ * What is this in this context?
+    Reason: Standalone Function Invocation.
+
+    You called the function simply as chai(). You did not call it as object.chai().
+
+    In JavaScript, when a regular function is called by itself, this defaults to the Global Object.
+
+    In a Browser: It prints the window object.
+
+    In Node.js: It prints the global object (which contains timers, setImmediate, etc.).
  */
 
 const chai = function(){
--- Functions/IIFE.js
+++ Functions/IIFE.js
@@ -2,8 +2,25 @@
 
 /**
  * We use because of the global polution problem 
- * The function executes after immediately Invoked Funcition Expression
+ * The function executes after Immediately Invoked Funcition Expression
+ * 
+ * Global Scope Pollution"
+        In JavaScript (especially in browsers), variables declared outside functions are Global. If you have two different files (e.g., script1.js and script2.js) and both create a variable named user, they will overwrite each other and crash your app.
+
  */
+/// 
+    // The variable 'connection' is destroyed immediately after running.
+    // It does NOT exist in the global scope.
+    // A. Data Privacy (Encapsulation)
+
+    // This was the primary way to create "private" variables before Classes were introduced. Variables defined inside the IIFE cannot be accessed from the outside.
+    // One-Time Initialization
+    // If you need to run setup code (like your "DB CONNECTED" example) once when the app starts, but you don't want to leave utility variables floating around in memory afterwards, an IIFE is perfect. It runs, does the job, and cleans up memory.
+
+    // Why use it? To create a private scope and execute code immediately without leaving a mess in the global memory.
+
+    // Is it still used? Yes, but less than before. Modern Modules (ES6 import/export) and Block Scope (let/const) solve the same problems more cleanly. However, you will still see IIFEs everywhere in older libraries and initialization scripts.
+/// 
 
 (function chai(){
     console.log("DB CONNECTD");
