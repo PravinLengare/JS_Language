2dd501927795d1f19b8f40f1eb87c3b26e2cc2e9
PravinLengare <lengarepravin115@gmail.com>
2026-06-17 15:40:45 +0530

    Practiced Numbers in the js

--- Numbers And Functions/basics.js
+++ Numbers And Functions/basics.js
@@ -1,33 +1,33 @@
-/*
 const balance = 1000;
 console.log(balance);
+console.log(typeof balance);
 
-const newNumber = new Number(24);
-console.log(newNumber.toString().length);
-console.log(newNumber.toFixed(1));
+
+const newNumber = new Number(32)
+console.log(newNumber);
 console.log(typeof newNumber);
-*/
+console.log(typeof newNumber.toString());
+console.log(newNumber.toFixed(2));
 
-/* 
-const otherNumber = 123.34545;
-console.log(otherNumber.toPrecision(5)); // how much precise the value should be
+const num = 123.89797
+console.log(num.toPrecision(4));        // how much precise the value should be
+
 
 const hundreds = 100000;
-console.log(hundreds.toLocaleString('en-IN'));  // it gives the indian std of the values 
+console.log(hundreds.toLocaleString('en-IN'));  // it gives the indian std of the values
 
-*/
+
 
 // ++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
 
-/*
+
 console.log(Math);
 console.log(Math.max(1,2,3,53,3));
 console.log(Math.min(1,2,3,53,3));
-console.log(Math.floor(3.72));
+console.log(Math.floor(3.722243243276866));
 console.log(Math.ceil(4.13243));
 console.log(Math.round(3.56));
 console.log(Math.abs(-2));
-*/
 
 let tiny = 0.00456789;
 console.log(tiny.toPrecision(2));
@@ -44,10 +44,9 @@ console.log( (Math.random()* 10) + 1);
 console.log(Math.floor( (Math.random()* 10) + 1));
 
 const min = 10
-const max = 20
-// console.log(Math.random()*(max - min)+1);
+const max = 30
 
-console.log(Math.floor((Math.random()*(max - min) + 1)) + min);
+console.log(Math.floor((Math.random()*(max - min)+1)) + min);
 
 
 
