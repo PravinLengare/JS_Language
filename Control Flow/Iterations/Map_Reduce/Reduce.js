929c58145eb54d763c7089da9074785e892412bc
PravinLengare <lengarepravin115@gmail.com>
2026-07-14 18:57:12 +0530

    Changes in the Reduce method

--- Control Flow/Iterations/Map_Reduce/Reduce.js
+++ Control Flow/Iterations/Map_Reduce/Reduce.js
@@ -1,12 +1,23 @@
 const myNums = [1, 2, 3]
 
-const result = myNums.reduce((acc,currVal)=>{
-   console.log(`acc: ${acc} and currval: ${currVal}`)
-   return acc + currVal
-    
+const result = myNums.reduce(function (acc,curr){
+    console.log(`${acc} and ${curr}`);
+    return acc + curr;
 },0)
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
-console.log("The result is : ",result,myTotal)
+// second way of writing 
+const resultT = myNums.reduce( (acc,curr) => {
+    return acc + curr
+},0)
+console.log(resultT);
+
+
+// const result = myNums.reduce((acc,currVal)=>{
+//    console.log(`acc: ${acc} and currval: ${currVal}`)
+//    return acc + currVal
+    
+// },0)
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// console.log("The result is : ",result,myTotal)
 
 
 const shoppingCart = [
@@ -27,6 +38,12 @@ const shoppingCart = [
         price: 12999
     },
 ]
+const totalPrice = shoppingCart.reduce(function (acc,curr){
+    return (acc + curr.price)
+},0)
+console.log(totalPrice);
+
+
 const priceToPay = shoppingCart.reduce( (num,item) => num + item.price ,0)
 
 console.log(priceToPay);
--- Control Flow/Iterations/Map_Reduce/tempCodeRunnerFile.js
+++ Control Flow/Iterations/Map_Reduce/tempCodeRunnerFile.js
@@ -1,4 +1,11 @@
-let result = books.filter( (book) =>{
-    book.genre === 'Science'&& book.edition === 2016
-})
-console.log(result);
+const myNums = [1, 2, 3]
+
+const result = myNums.reduce(function (acc,curr){
+    console.log(`${acc} and ${curr}`);
+    return acc + curr;
+},0)
+// second way of writing 
+const resultT = myNums.reduce( (acc,curr) => {
+    return acc + curr
+},0)
+console.log(resultT);
